2953c82e86d234b16da074084e3e35be2918d9b5
Matthew Bowen <Matthewbowenrd@gmail.com>
2024-01-25 23:29:42 -0500

    Delete requeststorageaccess.js

--- requeststorageaccess.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', event => {
-    document.getElementById('theButton').addEventListener("click", event => {
-
-        // Just go back to the outside iframe we came from.
-        document.cookie = "embeddedCookie=Cookie loaded; SameSite=None; Secure; path=/";
-        window.history.back();
-    });
-});
